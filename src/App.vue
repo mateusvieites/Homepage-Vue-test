359ea4ce8ad0d2e9a9b16f350b5e5ce9f68388d5
HardszVick / Nirin <48382353+mateusvieites@users.noreply.github.com>
2024-03-04 10:15:44 -0300

    Vue files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <PrimeiroComponente /> {{ casa }}
+</template>
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponente
+  },data(){
+    return{
+      casa: "mateus"
+    }
+  }
+}
+</script>
